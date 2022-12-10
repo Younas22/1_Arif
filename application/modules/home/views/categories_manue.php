196eaca76c1e51e8ffed0de54dadd962c746a3c3
Younas Mahmood <hm.younas22@gmail.com>
2022-12-10 22:44:38 +0500

    i

--- application/modules/home/views/categories_manue.php
+++
@@ -1,9 +0,0 @@
-<ul class="">
-  <?php foreach (blog_category() as $key) { ?> 
-    <li>
-        <div class="sidebar-meta">
-              <span class="time"><i class="fa fa-angle-right"></i> <a href="<?=base_url('blog/category/').str_replace(" ","-",strtolower($key->category_title))?>" class="text-dark text-decoration-none"><?=$key->category_title?></a></span>
-          </div>
-      </li>
-  <?php } ?>
-</ul>
--- application/modules/template/views/admin/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if ($contant_view == 'admin/sitemap') {
-		$this->load->view($contant_view);
-	}else{
-		$this->load->view('admin/header');
-		$this->load->view($contant_view);
-		$this->load->view('admin/footer');	
-	}
-
